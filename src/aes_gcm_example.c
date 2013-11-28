d2ab2128099ef65484331bb49d76aefe9efb7da6
stephan <stephan@localhost>
2013-11-28 13:49:55 +0100

    obtain GCM tag

--- src/aes_gcm_example.c
+++ src/aes_gcm_example.c
@@ -18,11 +18,12 @@ static EVP_CIPHER_CTX ctx;
 
 static const unsigned char key[AES_BLOCK_SIZE] =
   "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
-static const unsigned char plain[] =
+static const unsigned char plain[AES_BLOCK_SIZE] =
   "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
 static const unsigned char iv[] =
   "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
 static unsigned char cipher[AES_BLOCK_SIZE];
+static unsigned char tag[16];
 
 int main(void)
 {
@@ -33,8 +34,12 @@ int main(void)
   EVP_EncryptUpdate(&ctx, cipher, &out_length, plain, sizeof(plain));
   EVP_EncryptFinal(&ctx, garbage, &out_length);
 
+  EVP_CIPHER_CTX_ctrl(&ctx, EVP_CTRL_GCM_GET_TAG, 16, tag);
+
   EVP_CIPHER_CTX_cleanup(&ctx);
 
+  printf("expected cipher: 0388dace 60b6a392 f328c2b9 71b2fe78\n"
+         "got cipher:      ");
   size_t index;
   for (index = 0; index < sizeof(cipher) - 1; ++index) {
     printf("%02x", cipher[index]);
@@ -43,6 +48,16 @@ int main(void)
     }
   }
   printf("%02x\n", cipher[sizeof(cipher) - 1]);
+
+  printf("expected tag: ab6e47d4 2cec13bd f53a67b2 1257bddf\n"
+         "got tag:      ");
+  for (index = 0; index < sizeof(tag) - 1; ++index) {
+    printf("%02x", tag[index]);
+    if ((index + 1) % 4 == 0) {
+      printf(" ");
+    }
+  }
+  printf("%02x\n", tag[sizeof(tag) - 1]);
 
   return 0;
 }
